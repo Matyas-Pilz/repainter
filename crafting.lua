faa54f1efd88e8532490a98b5cbc459435ae7cf7
Matyáš Pilz <klanmptank@gmail.com>
2025-07-09 12:18:16 +0200

    Update crafting.lua

--- crafting.lua
+++ crafting.lua
@@ -1,76 +1,67 @@
-has_xcompat = minetest.get_modpath("xcompat") ~= nil
-has_mcl_core = minetest.get_modpath("mcl_core") ~= nil
-has_default = minetest.get_modpath("default") ~= nil
--- color configuration
+local has_xcompat = minetest.get_modpath("xcompat") ~= nil
+local has_mcl_core = minetest.get_modpath("mcl_core") ~= nil
+local has_default = minetest.get_modpath("default") ~= nil
+
 local color_assignment = {
     a = "blue",
     b = "red",
     c = "green",
     d = "yellow"
 }
-local wool_name
-if has_mcl_core then
-    wool_name = "mcl_wool:"
-else
-    wool_name = "wool:"
-end
 
-if has_xcompat then
-    minetest.register_craft({
-        output = "repainter:repainter_"..rtype,
-        recipe = {
-            { wool_name..color_assignment[rtype] },
-            { xcompat.materials["steel_ingot"] },
-            { xcompat.materials["stick"] }
-        }
-    })
-elseif has_mcl_core then
-    minetest.register_craft({
-        output = "repainter:repainter_"..rtype,
-        recipe = {
-            { "mcl_wool:"..color_assignment[rtype] },
-            { "mcl_core:iron_ingot" },
-            { "group:stick" }
-        }
-    })
-else
-    -- default
-    minetest.register_craft({
-        output = "repainter:repainter_"..rtype,
-        recipe = {
-            { "wool:"..color_assignment[rtype] },
-            { "default:steel_ingot" },
-            { "default:stick" }
-        }
-    })
-end
+local wool_name = has_mcl_core and "mcl_wool:" or "wool:"
 
-if has_xcompat then
+for _, def in ipairs(repainter_tool_defs or {}) do
+    local name = "repainter:" .. def.tooltype .. "_" .. def.rtype
+    local wool = wool_name .. color_assignment[def.color]
+
+    local recipe
+    if has_xcompat then
+        if def.tooltype == "repainter" then
+            recipe = {
+                { wool },
+                { xcompat.materials["steel_ingot"] },
+                { xcompat.materials["stick"] }
+            }
+        else
+            recipe = {
+                { xcompat.materials["steel_ingot"] },
+                { wool },
+                { xcompat.materials["steel_ingot"] }
+            }
+        end
+    elseif has_mcl_core then
+        if def.tooltype == "repainter" then
+            recipe = {
+                { wool },
+                { "mcl_core:iron_ingot" },
+                { "group:stick" }
+            }
+        else
+            recipe = {
+                { "mcl_core:iron_ingot" },
+                { wool },
+                { "mcl_core:iron_ingot" }
+            }
+        end
+    else
+        if def.tooltype == "repainter" then
+            recipe = {
+                { wool },
+                { "default:steel_ingot" },
+                { "default:stick" }
+            }
+        else
+            recipe = {
+                { "default:steel_ingot" },
+                { wool },
+                { "default:steel_ingot" }
+            }
+        end
+    end
+
     minetest.register_craft({
-        output = "repainter:rotator_"..rtype,
-        recipe = {
-            { xcompat.materials["steel_ingot"] },
-            { wool_name..color_assignment[rtype] },
-            { xcompat.materials["steel_ingot"] }
-        }
-    })
-elseif has_mcl_core then
-    minetest.register_craft({
-        output = "repainter:rotator_"..rtype,
-        recipe = {
-            { "mcl_core:iron_ingot" },
-            { "mcl_wool:"..color_assignment[rtype] },
-            { "mcl_core:iron_ingot" }
-        }
-    })
-else
-    -- default
-    minetest.register_craft({
-        output = "repainter:rotator_"..rtype,
-        recipe = {
-            { "default:steel_ingot" },
-            { "wool:"..color_assignment[rtype] },
-            { "default:steel_ingot" }
-        }
+        output = name,
+        recipe = recipe
     })
 end
